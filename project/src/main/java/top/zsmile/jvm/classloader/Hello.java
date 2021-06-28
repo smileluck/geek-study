88159839ec5e96b80310f54e000fc0e12dba2ec9
smileluck <drenchedzlove@163.com>
2021-06-28 10:51:06 +0800

    更新

--- project/src/main/java/top/zsmile/jvm/classloader/Hello.java
+++ project/src/main/java/top/zsmile/jvm/classloader/Hello.java
@@ -4,6 +4,23 @@ public class Hello {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        map_tx2bd(23.394300690824174,116.66392321577842);
     }
+
+    public static  double[] map_tx2bd(double lat, double lon){
+        double bd_lat;
+        double bd_lon;
+        double x_pi=3.14159265358979324;
+        double x = lon, y = lat;
+        double z = Math.sqrt(x * x + y * y) + 0.00002 * Math.sin(y * x_pi);
+        double theta = Math.atan2(y, x) + 0.000003 * Math.cos(x * x_pi);
+        bd_lon = z * Math.cos(theta) + 0.0065;
+        bd_lat = z * Math.sin(theta) + 0.006;
+
+        System.out.println("bd_lat:"+bd_lat);
+        System.out.println("bd_lon:"+bd_lon);
+
+        return new double[]{bd_lat,bd_lon};
+    }
+
 }
