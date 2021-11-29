8481323ce539ed5ffaad71c280e230c40362712a
smilex <drenchedzlove@163.com>
2021-11-29 22:44:13 +0800

    add  classloader manager

--- project/src/main/java/top/zsmile/utils/ClassLoaderManager.java
+++ project/src/main/java/top/zsmile/utils/ClassLoaderManager.java
@@ -1,21 +1,68 @@
 package top.zsmile.utils;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ClassLoaderManager {
 
     private static ClassLoaderManager manager = new ClassLoaderManager();
 
+    // common classloader
     private static CommonClassloader commonClassloader;
+
+    // save classloader
+    protected ConcurrentHashMap<String, Class<?>> loaderCache = new ConcurrentHashMap<>();
 
     private ClassLoaderManager() {
 //        CommonClassloader commonClassloader = new CommonClassloader();
     }
 
     static class CommonClassloader extends URLClassLoader {
+        // need decode class, have base64 decode
+        protected String decodeType = null;
+
         public CommonClassloader(URL[] urls) {
             super(urls);
+        }
+
+        public CommonClassloader(URL[] urls, String decodeType) {
+            this(urls);
+            this.decodeType = decodeType;
+        }
+
+        @Override
+        protected Class<?> findClass(String name) throws ClassNotFoundException {
+
+            InputStream resourceAsStream = this.getResourceAsStream(name);
+            try {
+                int available = resourceAsStream.available();
+                byte[] bytes = new byte[available];
+                resourceAsStream.read(bytes);
+                byte[] decode = ClassLoaderUtils.decodeClass.checkDecode(this.decodeType, bytes);
+                if (decode == null) {
+                    throw new ClassNotFoundException();
+                }
+                Class<?> aClass = defineClass(name, decode, 0, decode.length);
+                return aClass;
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new ClassNotFoundException();
+            } finally {
+                if (resourceAsStream != null) {
+                    try {
+                        resourceAsStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
         }
     }
 
--- project/src/main/java/top/zsmile/utils/ClassLoaderUtils.java
+++ project/src/main/java/top/zsmile/utils/ClassLoaderUtils.java
@@ -4,11 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ClassLoaderUtils {
+    static class decodeClass {
 
-    private static final Map classMap = new HashMap<>();
+        public static byte[] checkDecode(String decodeType, byte[] code) {
+            if (decodeType == null) {
+                return code;
+            }
+            byte[] decodeData = null;
+            switch (decodeType) {
+                case "base64":
+                    decodeData = base64Decode(code);
+                    break;
+            }
+            return decodeData;
+        }
 
-    public static void main(String[] args) {
-
+        private static byte[] base64Decode(byte[] code) {
+            byte[] decodeBytes = new byte[code.length];
+            for (int i = 0; i < code.length; i++) {
+                decodeBytes[i] = (byte) (255 - code[i]);
+            }
+            return decodeBytes;
+        }
     }
-
 }
